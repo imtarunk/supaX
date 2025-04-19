062530e70bee7b7e7e9114c64e0bf3972a4dfe34
imtarunk <tarunshr145@gmail.com>
2025-04-20 03:02:55 +0530

    Update .gitignore and remove .next from git tracking

--- .next/server/app/api/twitter/mentions/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/twitter/mentions/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_0544fde7._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__40b15ce3._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/twitter/mentions/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/twitter/mentions/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/twitter/mentions/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uZPILR1xpOMGjph4gc0/I4ILPyJgjGCjpbjeGMQQ/EM=\"\n}"
